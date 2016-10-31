2ac8b82f44312f7d5c7f25927a0aff248fc1f180
tqi_goliveira <tqi_goliveira@uolinc.com>
2016-10-31 16:25:41 -0200

    carregando pokemon

--- js/pokemonController.js
+++ js/pokemonController.js
@@ -1,4 +1,19 @@
 angular.module('pokedex')
-    .controller('pokemonController', function($scope, $routeParams, $http) {
-        $scope.pokemonName = $routeParams.name;
+    .controller('pokemonController', function($scope, $routeParams, $http, $templateCache) {
+        $scope.carregando = true;
+
+        $http.get("http://pokeapi.co/api/v2/pokemon/" + $routeParams.name, {
+            cache: $templateCache
+        }).
+        then(function(response) {
+            $scope.pokemon = response.data;
+            $scope.erro = false;
+            $scope.carregando = false;
+
+        }, function(response) {
+            $scope.erro = true;
+            $scope.data = response.data || 'Request failed';
+            $scope.status = response.status;
+            $scope.carregando = false;
+        });
     });
