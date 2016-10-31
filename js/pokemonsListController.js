0de9b373e88d9cdd3bda4a686b5af701d8ac35b3
tqi_goliveira <tqi_goliveira@uolinc.com>
2016-10-31 17:32:35 -0200

    paginação

--- js/pokemonsListController.js
+++ js/pokemonsListController.js
@@ -1,18 +1,22 @@
 angular.module('pokedex')
     .controller('pokemonsListController', function($scope, $http, $templateCache) {
-      $scope.carregando = true;
-
-      $http.get("http://pokeapi.co/api/v2/pokemon/", {
-          cache: $templateCache
-      }).
-      then(function(response) {
-          $scope.data = response.data;
-          $scope.erro = false;
-      }, function(response) {
-          $scope.erro = true;
-          $scope.data = response.data || 'Request failed';
-          $scope.status = response.status;
-      }).finally(function() {
-          $scope.carregando = false;
-      });
+        $scope.carregar = function(url) {
+            if (url) {
+                $scope.carregando = true;
+                $http.get(url, {
+                    cache: $templateCache
+                }).
+                then(function(response) {
+                    $scope.data = response.data;
+                    $scope.erro = false;
+                }, function(response) {
+                    $scope.erro = true;
+                    $scope.data = response.data || 'Request failed';
+                    $scope.status = response.status;
+                }).finally(function() {
+                    $scope.carregando = false;
+                });
+            }
+        }
+        $scope.carregar("http://pokeapi.co/api/v2/pokemon/");
     });
